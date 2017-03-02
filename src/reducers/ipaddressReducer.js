6d47d33a19dcddbeb45eb46f3af5437353adf51e
Stephen Wiggins <steve.wiggins7@gmail.com>
2017-03-02 12:50:20 -0500

    add some reducer code

--- src/reducers/ipaddressReducer.js
+++ src/reducers/ipaddressReducer.js
@@ -1,52 +1,18 @@
-
-//todo, fix all the things to ipaddress
-
 export default function reducer(state={
-    tweets: [],
+    ipaddresses: [],
     fetching: false,
     fetched: false,
-    error: null,
-  }, action) {
-
+    error: null
+}, action){
     switch (action.type) {
-      case "FETCH_TWEETS": {
-        return {...state, fetching: true}
-      }
-      case "FETCH_TWEETS_REJECTED": {
-        return {...state, fetching: false, error: action.payload}
-      }
-      case "FETCH_TWEETS_FULFILLED": {
-        return {
-          ...state,
-          fetching: false,
-          fetched: true,
-          tweets: action.payload,
+        case "FETCH_IPADDRESSES": {
+            return {...state, fetching: true}
         }
-      }
-      case "ADD_TWEET": {
-        return {
-          ...state,
-          tweets: [...state.tweets, action.payload],
+        case "FETCH_IPADDRESSES_REJECTED": {
+            return {...state, fetching: false, error: action.payload}
         }
-      }
-      case "UPDATE_TWEET": {
-        const { id, text } = action.payload
-        const newTweets = [...state.tweets]
-        const tweetToUpdate = newTweets.findIndex(tweet => tweet.id === id)
-        newTweets[tweetToUpdate] = action.payload;
-
-        return {
-          ...state,
-          tweets: newTweets,
+        case "FETCH_IPADDRESSES": {
+            return {...state, fetching: true}
         }
-      }
-      case "DELETE_TWEET": {
-        return {
-          ...state,
-          tweets: state.tweets.filter(tweet => tweet.id !== action.payload),
-        }
-      }
     }
-
-    return state
 }
